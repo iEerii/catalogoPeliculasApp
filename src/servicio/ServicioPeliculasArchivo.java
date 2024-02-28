4c882dd7dbb39aa9d2acbc10f5bd523089c61cb5
Eri <erickaponce92@gmail.com>
2024-02-28 14:41:01 -0500

    create listarPeliculas method into Archivo package

---
+++ src/servicio/ServicioPeliculasArchivo.java
@@ -0,0 +1,57 @@
+package servicio;
+
+import dominio.Pelicula;
+
+import java.io.*;
+
+public class ServicioPeliculasArchivo implements IServicioPeliculas{
+    private final String NOMBRE_ARCHIVO = "peliculas.txt";
+    public ServicioPeliculasArchivo(){
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{
+            //Para no crear dos veces un archivo existente
+            if(archivo.exists()){
+                System.out.println("El archivo ya existe");
+            } else {
+                var salida = new PrintWriter(new FileWriter(archivo));
+                salida.close();
+                System.out.println("Se ha guardado correctamente");
+            }
+        }catch (Exception e){
+            System.out.println("Ocurrio  un error al abrir el archivo" + e.getMessage());
+        }
+    }
+    @Override
+    public void listarPeliculas() {
+        //Para abrir el archivo que se cerró anteriormente
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{
+            System.out.println("Listado de Peliculas");
+            //Se abre el archivo para lectura
+            var entrada = new BufferedReader(new FileReader(archivo));
+            //Lectura linea por linea
+            String linea;
+            linea = entrada.readLine();
+            //Para leer todas las lineas
+            while(linea != null){
+                var pelicula = new Pelicula(linea);
+                System.out.println(pelicula);
+                //Se vuelve a escribir la siguiente linea antes de terminar el ciclo
+                linea = entrada.readLine();
+            }
+            entrada.close();
+        }catch(Exception e){
+            System.out.println("Ocurrio un error al leer el archivo" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void agregarPelicula(Pelicula pelicula) {
+
+    }
+
+    @Override
+    public void buscarPelicula(Pelicula pelicula) {
+
+    }
+}
